2c0fe615ddfb3b8c5934ab0ad97d0af8398ad202
Crepaldi93 <keevencrepaldi1@gmail.com>
2022-12-08 10:56:44 -0300

    first commit: all tests passing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+var fileUpload = require('express-fileupload');
 
 var app = express();
 
@@ -11,7 +12,29 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
 
+// Use body-parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+// Post file
+app.post("/api/fileanalyse", (req, res) => {
+  // Use the "upfile" name from index.html to get updated file
+  let myFile = req.files.upfile;
+
+  // Upload file to the "uploads" directory
+  myFile.mv("./uploads/" + myFile.name)
+
+  return res.json({
+    name: myFile.name,
+    type: myFile.mimetype,
+    size: myFile.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
